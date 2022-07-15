e93527964f3b3a06837383f16e9aec28de05d201
Jim Miller <james.miller319@gmail.com>
2022-07-15 13:23:58 +0000

    notes

--- modules/install_script_lib.tf
+++ modules/install_script_lib.tf
@@ -6,4 +6,14 @@ module "startup-script-lib" {
 metadata metadata = {
     startup-script        = module.startup-scripts.content
     startup-script-custom = file("${path.module}/files/startup-script-custom")
+
+
+    other option:
+
+    gcloud compute project-info add-metadata \
+  --project PROJECT_ID \
+  --metadata=enable-osconfig=TRUE
+gcloud compute instances add-metadata VM_NAME \
+  --metadata=enable-osconfig=TRUE
+
     */
