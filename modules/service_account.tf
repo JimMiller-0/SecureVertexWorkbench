f742851fa49f499d96770ab3f3c7e0cb664aec19
Jim Miller <james.miller319@gmail.com>
2023-04-28 13:27:11 +0000

    service account

--- modules/service_account.tf
+++ modules/service_account.tf
@@ -1,27 +1,17 @@
 # this template creates the necessary service account with least privleges for workbench
 
-/**
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+resource "google_service_account" "sa" {
+  project      = var.project_id
+  account_id   = var.account_id
+  display_name = var.description
 
-module "service_accounts" {
-  source        = "../.."
-  project_id    = var.project_id
-  prefix        = var.prefix
-  names         = ["single-account"]
-  project_roles = ["${var.project_id}=>roles/viewer"]
-  display_name  = "Single Account"
-  description   = "Single Account Description"
+}
+
+resource "google_project_iam_member" "sa_iam" {
+  for_each = toset(var.roles)
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.sa.email}"
+
 }
