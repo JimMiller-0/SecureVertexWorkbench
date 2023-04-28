dd15dfecfef398101de105b37e6846708b781f90
Jim Miller <james.miller319@gmail.com>
2023-04-28 14:37:15 +0000

    service account

--- modules/service_account.tf
+++ modules/service_account.tf
@@ -2,13 +2,14 @@
 
 resource "google_service_account" "sa" {
   project      = var.project_id
-  account_id   = var.account_id
-  display_name = var.description
+  account_id   = "securevertex-sa"
+  display_name = "securevertex-sa"
 
 }
 
 resource "google_project_iam_member" "sa_iam" {
-  for_each = toset(var.roles)
+  for_each = toset([role/compute.admin,
+                    role/serviceAccountUser])
 
   project = var.project_id
   role    = each.value
